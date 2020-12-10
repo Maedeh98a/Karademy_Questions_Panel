d6fb9f27af0287b8ea5cb8e61b751211ca24c952
Maedeh98a <maedeh.ahmadian98@gmail.com>
2020-12-10 11:20:14 -0500

    add modified model(add report)

--- questions/models.py
+++ questions/models.py
@@ -3,6 +3,19 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from django.utils.text import slugify
 
+STATUS_CHOICE = (
+    ('published', 'انتشاریافته'),
+    ('draft', 'پیش نویس'))
+
+REPORT_CHOICES = (
+    ('inappropiate', 'نامناسب'),
+    ('', 'بی ربط'),
+    ('', 'حاوی الفاظ سیاسی'),
+    ('', 'تبلیغ'),
+    ('other', ''),
+
+)
+
 
 class Category(models.Model):
     parent = models.ForeignKey(
@@ -49,6 +62,8 @@ class Question(models.Model):
     updated_time = models.DateTimeField(
         auto_now_add=True, null=True, db_index=True, verbose_name="تاریخ به روز رسانی"
     )
+    status = models.CharField(
+        max_length=12, choices=STATUS_CHOICE, default='published', null=True, verbose_name='وضعیت انتشار')
     question_photo = models.ImageField(
         upload_to="question/%Y/%m/%d", blank=True, null=True, verbose_name="تصویر سوال"
     )
@@ -83,6 +98,9 @@ class Answer(models.Model):
     answer = models.TextField(null=True, blank=False, verbose_name="پاسخ")
     created_time = models.DateTimeField(auto_now=True, null=True)
     updated_time = models.DateTimeField(auto_now=True, null=True)
+    status = models.CharField(
+        max_length=12, choices=STATUS_CHOICE, default='published', null=True,
+        verbose_name='وضعیت انتشار')
 
     class Meta:
         db_table = "Answers"
@@ -90,4 +108,15 @@ class Answer(models.Model):
         verbose_name_plural = "پاسخ ها"
 
     def __str__(self):
-        return self.question
+        return self.question.question_title
+
+
+class Report(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='گزارش دهنده')
+    reported_question = models.ForeignKey(Question, null=True, blank=True, on_delete=models.CASCADE, verbose_name='سوال گزارش شده')
+    # I got problem here ...
+    reported_answer = models.ForeignKey(Answer, null=True, blank=True, on_delete=models.CASCADE, verbose_name='پاسخ گزارش شده')
+    report_choice = models.CharField(max_length=30, choices=REPORT_CHOICES, blank=False, null=True, verbose_name='گزینه های گزارش')
+    report_description = models.TextField(blank=True, null=True, verbose_name='متن گزارش')
+    report_date = models.DateTimeField(auto_now=True, verbose_name='تاریخ گزارش')
+    report_count = models.IntegerField(verbose_name='تعداد گزارش')
